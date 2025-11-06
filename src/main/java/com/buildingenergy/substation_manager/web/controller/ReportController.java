27d91e1d40d6556c48333a0ee36866f236a7216c
Martin Dinev <martindinev10@gmail.com>
2025-11-06 00:40:09 +0000

    feat: add reports page and controller for viewing history

--- src/main/java/com/buildingenergy/substation_manager/web/controller/ReportController.java
+++ src/main/java/com/buildingenergy/substation_manager/web/controller/ReportController.java
@@ -1,5 +1,7 @@
 package com.buildingenergy.substation_manager.web.controller;
 
+import com.buildingenergy.substation_manager.meter.service.MeterHistoryService;
+import com.buildingenergy.substation_manager.reading.service.ReadingHistoryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,7 +9,15 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/reports")
-public class ReportsController {
+public class ReportController {
+
+    private final ReadingHistoryService readingHistoryService;
+    private final MeterHistoryService meterHistoryService;
+
+    public ReportController(ReadingHistoryService readingHistoryService, MeterHistoryService meterHistoryService) {
+        this.readingHistoryService = readingHistoryService;
+        this.meterHistoryService = meterHistoryService;
+    }
 
     @GetMapping
     public ModelAndView getReportsPage() {
@@ -15,6 +25,8 @@ public class ReportsController {
 
         modelAndView.setViewName("reports");
         modelAndView.addObject("currentPage", "reports");
+        modelAndView.addObject("readingHistory", readingHistoryService.getAll());
+        modelAndView.addObject("meterHistory", meterHistoryService.getAll());
 
         return modelAndView;
     }
