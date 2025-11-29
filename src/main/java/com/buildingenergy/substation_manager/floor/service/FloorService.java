2cb2d0b307236a415e1451a8d8883ee6cc625d70
Martin Dinev <martindinev10@gmail.com>
2025-11-29 18:06:45 +0000

    refactor: remove extra blank row in FloorService

--- src/main/java/com/buildingenergy/substation_manager/floor/service/FloorService.java
+++ src/main/java/com/buildingenergy/substation_manager/floor/service/FloorService.java
@@ -8,7 +8,6 @@ import com.buildingenergy.substation_manager.floor.repository.FloorRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
 @Service
 public class FloorService {
 
