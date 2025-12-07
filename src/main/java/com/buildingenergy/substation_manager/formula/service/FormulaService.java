137ad1e0e420c013d3ef573fb842e48a562be101
Martin Dinev <martindinev10@gmail.com>
2025-12-07 14:02:05 +0000

    refactor: remove @Cachable to prevent unwanted behaviour

--- src/main/java/com/buildingenergy/substation_manager/formula/service/FormulaService.java
+++ src/main/java/com/buildingenergy/substation_manager/formula/service/FormulaService.java
@@ -7,8 +7,6 @@ import com.buildingenergy.substation_manager.formula.dto.MeterFormulaRequest;
 import com.buildingenergy.substation_manager.formula.dto.MeterFormulaResponse;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -23,7 +21,6 @@ public class FormulaService {
         this.client = client;
     }
 
-    @Cacheable(value = "companyFormula", key = "#userId")
     public CompanyFormulaResponse getCompanyFormula(UUID userId) {
         try {
             return client.getCompanyFormula(userId).getBody();
@@ -33,7 +30,6 @@ public class FormulaService {
         }
     }
 
-    @CacheEvict(value = "companyFormula", key = "#userId")
     public boolean updateCompanyFormula(UUID userId, @Valid CompanyFormulaRequest request) {
         try {
             client.updateCompanyFormula(userId, request).getBody();
@@ -44,7 +40,6 @@ public class FormulaService {
         }
     }
 
-    @Cacheable(value = "meterFormula", key = "#userId")
     public MeterFormulaResponse getMeterFormula(UUID userId) {
         try {
             return client.getMeterFormula(userId).getBody();
@@ -54,7 +49,6 @@ public class FormulaService {
         }
     }
 
-    @CacheEvict(value = "meterFormula", key = "#userId")
     public boolean updateMeterFormula(UUID userId, @Valid MeterFormulaRequest request) {
         try {
             client.updateMeterFormula(userId, request).getBody();
